9f9272e18b991b39c89c0741bd2dadb24775fa9c
chenhao <c1376479276@163.com>
2021-11-15 09:20:39 +0800

    修改循环条件，新增变量加法运算

--- test.sh
+++ test.sh
@@ -1,17 +1,20 @@
-while(true)
+count=0
+echo "1 for while"
+echo "0 for exit"
+while(((count<3)&&(true)))
 do
 	echo "please input your choose!"
 	read choose
 	if [ $choose = 1 ]
 	then
 		echo "your choose $choose"
+		ls -la
 	elif [ $choose = 0 ]
 	then
 		echo "your choose $choose"
 		echo "exit"
 		exit
 	else
-		pwd
-		ls -la
+		count=$(($count+1))
 	fi
 done
